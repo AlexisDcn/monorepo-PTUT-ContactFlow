10c6824c58a31cd406beb9b7c4661c3e2a56c83b
Lina <lina.khadly@etud.univ-jfc.fr>
2025-02-06 09:10:39 +0100

    Classes Salon et Formation

---
+++ backend/src/main/java/isis/projet/backend/entity/Salon.java
@@ -0,0 +1,29 @@
+package isis.projet.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
+@Entity
+public class Salon {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id ;
+
+@NonNull
+@NotBlank
+@NotEmpty
+    private String name ;
+
+
+
+
+}
