32a88551672da8eba41050d7af2ebe41e96574f6
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2025-02-18 15:49:03 +0100

    suppr des fichiers inutiles et changement du style de certains components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
     <img alt="ISIS logo" class="logo" src="@/assets/logo-ISIS-vertical-RVB.svg" width="125" height="125" />
-
+    <h1 class="white">Contact’Flow</h1>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">Les pays</RouterLink>
@@ -43,29 +40,25 @@ header {
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  justify-content: space-around;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  color: black; /* Définit la couleur du texte en noir */
+  text-decoration: none; /* Enlève le soulignement */
 }
 
-nav a:first-of-type {
-  border: 0;
+nav a:hover {
+  color: gray; /* Optionnel : Change la couleur au survol */
 }
+
+nav a.router-link-exact-active {
+  color: black; /* Assure que le lien actif reste noir */
+  background-color: gray;
+}
+
+
+
 
 @media (min-width: 1024px) {
   header {
@@ -78,19 +71,8 @@ nav a:first-of-type {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+
 }
 </style>
