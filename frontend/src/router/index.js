9adfcd835e7ac7ca25441468c46df32337ec933f
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2025-03-13 12:06:11 +0100

    changement router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: ProspectForm,
+      component: ()=> import('../views/HomeView.vue')
     },
     {
       path: '/admin',
