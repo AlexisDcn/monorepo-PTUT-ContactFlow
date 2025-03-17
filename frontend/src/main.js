5f39343fc4836c292d1cd74658d8f28d41529a02
Jules-Pelissou <jules.pelissou@gmail.com>
2025-03-17 16:42:20 +0100

    Ajout ApexChart

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import VueApexCharts from "vue3-apexcharts"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+app.use(VueApexCharts);
 
 app.mount('#app')
