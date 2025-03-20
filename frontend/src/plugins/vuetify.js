7c7826a1eb236d678fa5875366a577f619bba8ac
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2025-03-20 10:57:35 +0100

    changement vuetify.js (ça a rien changé mais bon)

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -14,11 +14,15 @@ const vuetify = createVuetify({
           1: '#2f2769',
           2: '#5f4e9b',
           3: '#9059a0',
-          4: '#ed6962'
-        }
-      }
-    }
-  }
-})
+          4: '#ed6962',
+          paginationBackground: '#5f4e9b', // Fond de la pagination
+          paginationText: '#ffffff', // Couleur du texte
+          paginationActive: '#ed6962', // Couleur de la page active
+        },
+      },
+    },
+  },
+});
+
 
 export default vuetify;
