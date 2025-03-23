83a98bd1528b0ef891aa5815c2c1c27a0ea7b67b
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2025-03-23 21:26:16 +0100

    colonnes sortable

--- frontend/src/components/StatsGen.vue
+++ frontend/src/components/StatsGen.vue
@@ -1,10 +1,11 @@
 <template>
   <v-container class="container fill-height">
+
     <CsvDownloader :data="data.prospects" :headers="headers" />
-    <v-data-table :items="data.prospects" :footer-props="{
-    itemsPerPageOptions: [5, 10, 15, 20],
-    itemsPerPageText: 'Prospects par page :'
-  }">
+    <v-data-table dense
+                  :items="data.prospects"
+                  hide-default-footer
+                  class="elevation-1">
     </v-data-table>
   </v-container>
 </template>
@@ -43,15 +44,15 @@ const headers = [
 
 function getProspect() {
   doAjaxRequest("/api/prospects")
-    .then((result) => {
-      // Filtrer les données pour exclure la propriété _links
-      data.prospects = result._embedded.prospects.map(prospect => {
-        const { _links, ...rest } = prospect;
-        return rest;
-      });
-      console.log(data.prospects);
-    })
-    .catch((error) => alert(error.message));
+      .then((result) => {
+        // Filtrer les données pour exclure la propriété _links
+        data.prospects = result._embedded.prospects.map(prospect => {
+          const { _links, ...rest } = prospect;
+          return rest;
+        });
+        console.log(data.prospects);
+      })
+      .catch((error) => alert(error.message));
 }
 
 
@@ -60,12 +61,10 @@ onMounted(getProspect);
 
 <style scoped>
 .container {
-  margin: 2rem auto;
   max-width: 100%;
   background-color: #f9f9f9;
   padding: 20px;
   border-radius: 10px;
-  box-shadow: 0px 4px 10px #5f4e9b;
   overflow-x: auto;
 }
 
@@ -76,7 +75,7 @@ onMounted(getProspect);
 ::v-deep(.v-data-table) {
   background-color: #5F4E9B; /* Fond violet */
   border-radius: 15px; /* Coins arrondis */
-  padding: 20px; /* Espacement intérieur */
+  padding: 20px;
 }
 
 ::v-deep(.v-data-table-header) {
@@ -86,27 +85,14 @@ onMounted(getProspect);
   font-size: 16px;
 }
 ::v-deep(.v-data-table td) {
-  background-color: white; /* Fond blanc */
-  padding: 12px 15px;
-  text-align: left;
+  background-color: white;
+  text-align: center;
   font-size: 14px;
-  border: none;
-}
-::v-deep(.v-data-table tbody tr) {
-  border-bottom: none;
 }
 
-::v-deep(.v-data-table-footer) {
-  background-color: #5F4E9B;
-  color: white;
-}
+
 ::v-deep(.v-data-table thead th) {
   color: white !important;
-  font-weight: bold;
-  text-align: left;
+  text-align: center !important;
 }
-
-
-
-
 </style>
