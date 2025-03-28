4c9cdaf5943d474fdebe9c7b4eac42e9321491db
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2025-03-28 08:27:42 +0100

    ajout du Footer partout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,9 +4,11 @@
     <v-main>
       <router-view />
     </v-main>
+    <FooterGeneral />
   </v-app>
 </template>
 
 <script setup>
 import AppHeader from '@/components/AppHeader.vue';
+import FooterGeneral from "@/components/FooterGeneral.vue";
 </script>
