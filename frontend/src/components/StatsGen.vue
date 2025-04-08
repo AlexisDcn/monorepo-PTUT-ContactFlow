d6d8be165e45586c12aa2d39c05048579876abbf
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2025-04-08 15:03:50 +0200

    changement pagination datatable

--- frontend/src/components/StatsGen.vue
+++ frontend/src/components/StatsGen.vue
@@ -112,7 +112,7 @@
       </div>
 
       <div>
-        Affichage de {{ startItem }}-{{ endItem + 1 }} sur {{ totalItems + 1 }} éléments
+        Affichage de {{ startItem }}-{{ endItem }} sur {{ totalItems }} éléments
       </div>
     </div>
   </div>
